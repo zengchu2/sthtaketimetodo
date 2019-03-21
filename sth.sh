6f508e6066158b68168564e290e3b43e05651a7d
root <root@DESKTOP-9AMNB8N.localdomain>
2019-03-21 13:14:38 -0400

    Initial

---
+++ sth.sh
@@ -0,0 +1,6 @@
+echo "# cvmfsgsoc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/zengchu2/cvmfsgsoc.git
+git push -u origin master
